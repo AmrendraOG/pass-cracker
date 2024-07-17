e68b83e2ba64e5182ec619768035c50326437aaa
Amrendra Maurya <125346970+AmrendraOG@users.noreply.github.com>
2024-07-18 01:38:42 +0530

    Initial Commits

---
+++ cracker.py
@@ -0,0 +1,17 @@
+import zipfile
+
+
+def main():
+	file_name = 'Pictures.zip'
+	passfile = open('passwords.txt', 'r')
+	for word in passfile.readlines():
+		password = word.strip()
+		with zipfile.ZipFile(file_name) as file:
+			file.extractall(pwd = bytes(password, 'utf-8'))
+		print('Password Found: ' + password)
+		return
+	print('Sorry! password not found')
+
+
+if __name__ == '__main__':
+	main()
